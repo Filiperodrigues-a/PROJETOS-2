e95e375260b83a38bb92c647a0842cbd025fe6f2
Filiperodrigues-a <166249999+Filiperodrigues-a@users.noreply.github.com>
2025-01-19 21:56:47 -0300

    Add files via upload

---
+++ Sandra ele ela site html/js/main.js
@@ -0,0 +1,18 @@
+$('.banner-men').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.banner-women').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $('.banner-party').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
